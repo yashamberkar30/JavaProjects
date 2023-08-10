46c60a81a52f301a6c2386ed7f84621b5c8257e8
yashamberkar30 <140728758+yashamberkar30@users.noreply.github.com>
2023-08-10 10:14:07 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 19, b = 7, sum, sub, div, mul,mod;
+		
+		sum = a + b;
+		sub = a - b;
+		mul = a * b;
+		div = a/ b;
+		mod = a % b;
+		 
+		System.out.println("Subtraction of two " + sum);
+		System.out.println("Subtraction of two " + sub);
+		System.out.println("Subtraction of two " + mul);
+		System.out.println("Subtraction of two " + div);
+		System.out.println("Subtraction of two " + mod);
+
+	}
+
+}
